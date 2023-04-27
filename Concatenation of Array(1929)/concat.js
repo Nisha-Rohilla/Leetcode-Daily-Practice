905bf251e37498f30c5022a36231eae2d8099949
Nisha Rohilla <rohilla.nisha15@gmail.com>
2023-04-27 23:54:06 +0530

    concatenation of  array

---
+++ Concatenation of Array(1929)/concat.js
@@ -0,0 +1,26 @@
+// With the help of 2 for loop
+var getConcatenation = function(nums) {
+    const result = [];
+    for(let i=0; i<nums.length; i++){
+        result[i] = nums[i]
+    }
+    for(let i=0; i<nums.length; i++){
+        result[i+nums.length]=nums[i]
+    }
+    return result
+
+    
+};
+
+// with the help of one loop
+
+var getConcatenation = function(nums) {
+    const result = [];
+    for(let i=0; i<nums.length; i++){
+        result[i] = nums[i]
+        result[i+nums.length]=nums[i]
+    }
+    return result
+
+    
+};
