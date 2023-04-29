04e5df91d598a24e037d2cfecdf253be1eb90350
Nisha Rohilla <rohilla.nisha15@gmail.com>
2023-04-29 23:26:27 +0530

    Good Pairs

--- Number of Good Pairs(1512)/pairs.js
+++ Number of Good Pairs(1512)/pairs.js
@@ -7,7 +7,7 @@ var numIdenticalPairs = function(nums) {
     for(let i=0; i<nums.length; i++){
         for(let j=i+1; j<nums.length; j++){
         if(nums[i] == nums[j]){
-            count++
+            count++;
         }
         }
     } 
